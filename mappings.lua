d036a9043d74df00aefbe8a3798549e9a5f33633
Pham Huy Hoang <hoangtun0810@gmail.com>
2023-02-14 19:05:40 +0700

    Dap keybinds

--- mappings.lua
+++ mappings.lua
@@ -9,4 +9,38 @@ M.general = {
 
 -- more keybinds!
 
+--- Keybinds for nvim-dap that mimics VsCode debug keybinds
+M.dap = {
+  n = {
+    ["<F5>"] = {
+      "<cmd>DapContinue<CR>", "Start/Continue a debug session"
+    },
+    ["<F17>"] = { -- Keycode for Shift-f5
+      "<cmd>DapTerminate<CR>",
+      "Terminate/Stop a debug session"
+    },
+    ["<F6>"] = {
+      function()
+        require("dap").pause()
+      end,
+      "Pause a debug session"
+    },
+    ["<F9>"] = {
+      "<cmd>DapToggleBreakpoint<CR>",
+      "Toggle Breakpoint on Current Line"
+    },
+    ["<F10>"] = {
+      "<cmd>DapStepOver<CR>",
+      "Step Over (Debug session)",
+    },
+    ["<F11>"] = {
+      "<cmd>DapStepInto<CR>", "Step Into (Debug)"
+    },
+    ["<F23>"] = {
+      "<cmd>DapStepOut<CR>",
+      "Step Out (Debug)"
+    },
+  }
+}
+
 return M
